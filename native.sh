3cc46b25549681e0fc5a68d79e5030ef96f3db39
Josh Long <josh@joshlong.com>
2022-02-23 16:42:38 -0800

    graphql

--- native.sh
+++ native.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
 
-mvn -f ../hints/pom.xml clean install && mvn -X  -Pnative -DskipTests clean package
+mvn -DskipTests  -f ../hints/pom.xml spring-javaformat:apply clean install
+mvn -X  -Pnative -DskipTests clean package
--- src/main/java/com/example/graphqlnative/GraphqlNativeApplication.java
+++ src/main/java/com/example/graphqlnative/GraphqlNativeApplication.java
@@ -7,31 +7,34 @@ import org.springframework.core.NativeDetector;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.graphql.boot.GraphQlSourceBuilderCustomizer;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.execution.GraphQlSource;
 import org.springframework.stereotype.Controller;
 
 /**
  * @author Josh Long
  */
-@Controller
 @SpringBootApplication
 public class GraphqlNativeApplication {
 
     @Bean
     GraphQlSourceBuilderCustomizer graphQlSourceBuilderCustomizer() {
         return builder -> {
-			if (NativeDetector.inNativeImage())
-				builder.schemaResources(new ClassPathResource("graphql/schema.graphqls"));
-		};
+            if (NativeDetector.inNativeImage())
+                builder.schemaResources(new ClassPathResource("graphql/schema.graphqls"));
+        };
     }
 
-    @QueryMapping
-    String hello() {
-        return "hello, world!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GraphqlNativeApplication.class, args);
     }
 
+}
+
+@Controller
+class QueryController {
+
+    @QueryMapping
+    String hello() {
+        return "hello, world!";
+    }
 }
